3217cd6af3923f7a35a3541c71aa8ec88651dad5
DESKTOP-I16K27A\Nqobani Zulu <nqobani@immedia.co.za>
2018-08-10 11:53:32 +0200

    jsjsjs

---
+++ DMNS/Models/Imagesss.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DMNS.Models
+{
+    public class Imagesss
+    {
+       public string imageData { get; set; }
+    }
+}
